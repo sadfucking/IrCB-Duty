5620b386bf534ed2873c71a9cc51c561a158151f
sadfucking <80053927+sadfucking@users.noreply.github.com>
2021-09-03 22:50:59 +0300

    Update ping.py

--- idm/my_signals/ping.py
+++ idm/my_signals/ping.py
@@ -1,7 +1,9 @@
 from idm.objects import dp, MySignalEvent, LongpollEvent
 from datetime import datetime
+import time
 
 pings = {
+    "п":  "",
     "пинг": "ПОНГ",
     "кинг": "КОНГ",
     "пиу": "ПАУ",
@@ -23,6 +25,9 @@ def ping(event: MySignalEvent) -> str:
             обработано=round(datetime.now().timestamp() - event.time - event.vk_response_time, 2),  # noqa
             пингвк=round(event.vk_response_time, 2)
         ))
+    time.sleep(5)
+    event.msg_op(2, 'понг')
+    event.msg_op(3)
     return "ok"
 
 
@@ -36,5 +41,8 @@ def ping_bf(event: MySignalEvent) -> str:
 def ping_lp(event: LongpollEvent) -> str:
     c_time = datetime.now().timestamp()
     delta = round(c_time - event.msg['date'], 2)
-    event.msg_op(2, f'{pings.get(event.command)} LP\nОтвет через {delta}сек.')
+    event.msg_op(2, f'{pings.get(event.command)} LP\n {delta}сек.')
+    time.sleep(5)
+    event.msg_op(2, 'понг')
+    event.msg_op(3)
     return "ok"
